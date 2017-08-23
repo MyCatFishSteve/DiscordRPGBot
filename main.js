86dbc42979dfeaa98df822819494ff0e028ead4f
James Brooks <jallenby37@gmail.com>
2017-08-23 03:26:10 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var grabbed = false;
+var rpgClient = null;
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+	
+	if (message.author.username == "DiscordRPG" && message.author.discriminator == "0366" && grabbed == false) {
+		
+		console.log("Recorded DiscordRPG ");
+		rpgClient = message.author;
+		grabbed = true;
+		
+		for ( var i = 0 ; i < 288 ; i++ ) {	
+			setTimeout(function() {
+				message.author.send("#!chop")
+				message.author.send("#!forage")
+				message.author.send("#!mine")
+			}, i * 310000);
+		}
+		
+	}
+	
+});
+
+// Log the bot into the server
+client.login("");
